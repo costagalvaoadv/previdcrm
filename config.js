2d13dc975025eeedea22f1eb24c28c857afa2ae9
Jucineide Costa <jucineidecostagalvao@gmail.com>
2026-05-19 00:02:41 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = https://lpwyolzzawsvbswgvnsg.supabase.co;
-const SUPABASE_KEY = sb_publishable_7Zaxsnt3JpxAgmjBrMyyXA_U6RIRQf3;
+const SUPABASE_URL = 'https://lpwyolzzawsvbswgvnsg.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_7Zaxsnt3JpxAgmjBrMyyXA_U6RIRQf3';
 
 const { createClient } = supabase;
 const db = createClient(SUPABASE_URL, SUPABASE_KEY);
